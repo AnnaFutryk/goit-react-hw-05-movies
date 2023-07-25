703414573cede4aec5e5e241aa4c27179b2ac1ea
AnnaFutryk <anna.futryk@gmail.com>
2023-07-25 23:20:55 +0300

    Create MoviesPage.jsx

---
+++ src/components/page/MoviesPage.jsx
@@ -0,0 +1,17 @@
+// import { Link } from 'react-router-dom';
+
+const MoviesPage = () => {
+  return (
+    <>
+      <div>MoviesPage</div>
+      {/* <Link to={movies.id}>
+        <ul>
+          <li>test1</li>
+          <li>test2</li>
+          <li>test3</li>
+        </ul>
+      </Link> */}
+    </>
+  );
+};
+export default MoviesPage;
