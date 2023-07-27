b7fd78eacc8ea505b3407051185b5b2ce5ee62dd
AnnaFutryk <anna.futryk@gmail.com>
2023-07-28 01:39:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
-import MovieDetails from './MovieDetails/MovieDetails';
+import MovieDetails from '../pages/MovieDetails/MovieDetails';
 import HomePage from 'pages/HomePage/HomePage';
 import MoviesPage from 'pages/MoviesPage/MoviesPage';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
 
 const App = () => {
   return (
@@ -10,7 +12,10 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="movies" element={<MoviesPage />} />
-        <Route path="movies/:movieId" element={<MovieDetails />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
       </Route>
     </Routes>
   );
