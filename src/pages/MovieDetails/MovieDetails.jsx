726491b8d5d45171da9116c9634ac5917f17f7c2
AnnaFutryk <anna.futryk@gmail.com>
2023-07-28 18:09:53 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,9 @@
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { Suspense, useRef } from 'react';
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 
 const MovieDetails = () => {
+  const location = useLocation();
+  const backLinkLocationRef = useRef(location.state?.from ?? '/movies'); //для коректного повернення з вкладеного маршруту (з акторів та відгуків); зберігаємо локейшн з якого прийшли
   const { movieId } = useParams();
 
   // useEffect(() => {
@@ -10,6 +13,8 @@ const MovieDetails = () => {
   return (
     <>
       <h1>MovieDetails: {movieId}</h1>
+      <Link to={backLinkLocationRef.current}>Go back</Link>{' '}
+      {/* якщо з нової вкладки переходимо, то відкриється ст з /movies */}
       <ul>
         <li>
           <Link to="cast">Cast</Link>
@@ -18,7 +23,9 @@ const MovieDetails = () => {
           <Link to="reviews">Reviews</Link>
         </li>
       </ul>
-      <Outlet />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
     </>
   );
 };
