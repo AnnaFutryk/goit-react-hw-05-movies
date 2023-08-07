0e738e96475c86ff30ca113ee3af124c90c5ba96
AnnaFutryk <anna.futryk@gmail.com>
2023-08-08 02:29:15 +0300

    Update HomePage.styled.js

--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -2,12 +2,21 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Title = styled.h1`
+  @media screen and (max-width: 475.99px) {
+    font-size: 36px;
+  }
   margin-top: 90px;
   text-align: center;
   font-size: 46px;
 `;
 
 export const List = styled.ul`
+  @media screen and (max-width: 475.99px) {
+    display: flex;
+    flex-direction: column;
+    gap: 28px;
+    align-items: center;
+  }
   display: grid;
   max-width: calc(100vw - 48px);
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
